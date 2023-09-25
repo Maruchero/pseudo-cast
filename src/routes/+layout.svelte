d4e617efd394607a0727a9b8d8ddd75ca28e1914
Maruchero <marce.gnz303@gmail.com>
2023-09-25 10:10:15 +0200

    Init repository

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import '../fonts.css';
+</script>
+
+<slot />
+
+<style>
+    :global(body) {
+        font-family: "Source Code Pro";
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+	let pseudocodifica: string;
+	pseudocodifica = `RIPETI
+FINCHE' CIAO = true
+    Azione1
+    Azione2
+
+    SE EOF AND TEMPERATURA > 10
+    ALLORA
+        Azione1
+    ALTRIMENTI
+        Azione2
+        Azione3
+    FINE-SE
+FINE-RIPETI`;
+
+	let titolo: string;
+	titolo = 'Test';
+</script>
+
+<!-- HTML -->
+<h1>Pseudo-Carta-Strutturata</h1>
+
+<form method="post">
+	<input type="text" name="titolo" placeholder="Titolo" bind:value={titolo} />
+	<textarea name="pseudocodifica" cols="30" rows="20" bind:value={pseudocodifica} />
+	<button type="submit">Genera Carta Strutturata</button>
+</form>
+
+<!-- STYLE -->
+<style>
+	input {
+		box-sizing: border-box;
+		width: 100%;
+		margin-bottom: 5px;
+	}
+
+	textarea {
+		box-sizing: border-box;
+		width: 100%;
+	}
+</style>
