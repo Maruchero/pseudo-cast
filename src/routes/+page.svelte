1b33d5a0ca18d9f9f9a603a8412123e0c743a6da
Maruchero <marce.gnz303@gmail.com>
2023-10-09 15:42:33 +0200

    Graphic restyling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,56 +1,61 @@
 <script lang="ts">
-	let pseudocodifica: string;
-// 	pseudocodifica = `RIPETI
-// FINCHE' CIAO = true
-//     Azione1
-//     Azione2
+	import styles from './+page.css';
 
-//     SE EOF AND TEMPERATURA > 10
-//     ALLORA
-//         Azione1
-//     ALTRIMENTI
-//         Azione2
-//         Azione3
-//     FINE-SE
-// FINE-RIPETI`;
-pseudocodifica = `Imposta var = 0
-SE var < 4
-ALLORA
-    Azione1
-    Azione2
-ALTRIMENTI
-    Azione3
-FINE-SE`;
+	let pseudocodifica: string;
+	// pseudocodifica = `INIZIO
+	// Apertura File I/O
+	// Lettura fuori ciclo record INPUT
+
+	// SE EOF
+	// ALLORA
+	//     Stampa "Archivio Vuoto"
+	// ALTRIMENTI
+	//     RIPETI
+	//     FINCHE' CIAO = true
+	//         Azione1
+	//         Azione2
+
+	//         SE EOF AND TEMPERATURA > 10
+	//         ALLORA
+	//             Azione1
+	//         ALTRIMENTI
+	//             Azione2
+	//             Azione3
+	//         FINE-SE
+	//     FINE-RIPETI
+	// FINE-SE
+	// Chiusura file I/O
+	// FINE`;
 
 	let titolo: string;
-	titolo = 'Test';
+	// titolo = 'Test';
 
-    let author: string;
-    author = "Garonzi Marcello";
+	let author: string;
+	// author = "Garonzi Marcello";
 </script>
 
 <!-- HTML -->
-<h1>Pseudo-Carta-Strutturata</h1>
+<h1>Pseudo Carta Strutturata</h1>
 
 <form method="post" action="/download">
+	<img src="/user.png" alt="" />
 	<input type="text" name="autore" placeholder="Autore" bind:value={author} />
+	<img src="/rename.png" alt="" />
 	<input type="text" name="titolo" placeholder="Titolo" bind:value={titolo} />
-	<textarea name="pseudocodifica" cols="30" rows="20" bind:value={pseudocodifica} />
-	<button type="submit">Genera Carta Strutturata</button>
+	<img src="/code.png" alt="" />
+	<textarea
+		name="pseudocodifica"
+		cols="30"
+		rows="20"
+		placeholder="Pseudo-codice"
+		bind:value={pseudocodifica}
+	/>
+	<span class="empty" />
+	
+	<div class="submit-button">
+		<img src="/excel-icon.svg" alt="" />
+		<button type="submit">
+			Genera Carta Strutturata
+		</button>
+	</div>
 </form>
-
-<!-- STYLE -->
-<style>
-	input {
-		box-sizing: border-box;
-		width: 100%;
-		margin-bottom: 5px;
-        font-family: inherit;
-	}
-
-	textarea {
-		box-sizing: border-box;
-		width: 100%;
-        font-family: inherit;
-	}
-</style>
